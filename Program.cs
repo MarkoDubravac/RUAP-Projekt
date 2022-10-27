e8d372aac29183d9e550793cc1488e625b094710
“ivan.sekeres” <isekeres@etfos.hr>
2022-10-27 17:43:01 +0200

    Zadnji

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdrav iz aplikacije od Marka Dubravca, indeks: 444433332222");
+
         }
     }
 }
